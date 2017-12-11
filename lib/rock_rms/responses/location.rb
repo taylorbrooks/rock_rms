a27f70065e1d4c157980c59359a27fe2633c72b1
Justin Powell <jpowellcs@gmail.com>
2017-12-11 17:51:55 -0600

    adds support for locations

---
+++ lib/rock_rms/responses/location.rb
@@ -0,0 +1,37 @@
+module RockRMS
+  module Responses
+    class Location
+      MAP = {
+        id: 'Id',
+        name: 'Name',
+        is_active: 'IsActive',
+        street_1: 'Street1',
+        street_2: 'Street2',
+        city: 'City',
+        county: 'County',
+        state: 'State',
+        country: 'Country',
+        postal_code: 'PostalCode',
+        latitude: 'Latitude',
+        longitude: 'Longitude',
+        guid: 'Guid'
+      }.freeze
+
+      def self.format(data)
+        if data.is_a?(Array)
+          data.map { |object| format_single(object) }
+        else
+          format_single(data)
+        end
+      end
+
+      private
+
+      def self.format_single(data)
+        MAP.each.with_object({}) do |(l,r), object|
+          object[l] = data[r]
+        end
+      end
+    end
+  end
+end
