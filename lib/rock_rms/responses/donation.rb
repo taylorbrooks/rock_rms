d5e435e533b13a3babd5722b28278cc9bedaf2d7
Josh Crews <josh@joshcrews.com>
2018-01-24 15:21:47 -0600

    add specs to donation resource/responses

--- lib/rock_rms/responses/donation.rb
+++ lib/rock_rms/responses/donation.rb
@@ -1,24 +1,26 @@
 module RockRMS
-  class Donation
-    def self.format(response)
-      if response.is_a?(Array)
-        response.map { |donation| format_donation(donation) }
-      else
-        format_donation(response)
+  module Responses
+    class Donation
+      def self.format(response)
+        if response.is_a?(Array)
+          response.map { |donation| format_donation(donation) }
+        else
+          format_donation(response)
+        end
       end
-    end
 
-    def self.format_donation(donation)
-      {
-        id:     donation['Id'],
-        date:   donation['TransactionDateTime'],
-        amount: donation['TransactionDetails'].reduce(0) { |sum, td| sum + td['Amount'] },
-        person_id: donation['AuthorizedPersonAliasId'],
-        fund:   '',
-        batch_id: donation['BatchId'],
-        scheduled_transaction_id: donation['ScheduledTransactionId'],
-        summary: donation['Summary']
-      }
+      def self.format_donation(donation)
+        {
+          id:     donation['Id'],
+          date:   donation['TransactionDateTime'],
+          amount: donation['TransactionDetails'].reduce(0) { |sum, td| sum + td['Amount'] },
+          person_id: donation['AuthorizedPersonAliasId'],
+          fund:   '',
+          batch_id: donation['BatchId'],
+          scheduled_transaction_id: donation['ScheduledTransactionId'],
+          summary: donation['Summary']
+        }
+      end
     end
   end
 end
