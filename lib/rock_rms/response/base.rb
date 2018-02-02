00100dcdee500656a0c8169869087e8b0199e4ab
Taylor Brooks <tbrooks@gmail.com>
2018-02-02 14:39:48 -0600

    Guard against nil data responses

--- lib/rock_rms/response/base.rb
+++ lib/rock_rms/response/base.rb
@@ -20,6 +20,7 @@ module RockRMS
       end
 
       def to_h(dict, data)
+        return {} if data.nil?
         dict.each_with_object({}) do |(l, r), object|
           object[l] = data[r]
         end
