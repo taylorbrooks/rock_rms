436a90d3a4577397de262890c00d9ef431caf34c
Josh Crews <josh@joshcrews.com>
2018-02-24 11:43:30 -0600

    handle nil batch start_date parse

--- lib/rock_rms/response/batch.rb
+++ lib/rock_rms/response/batch.rb
@@ -17,13 +17,21 @@ module RockRMS
         response = to_h(MAP, data)
         response[:transactions] = Transaction.format(response[:transactions])
         response[:amount]       = calculate_total(response[:transactions])
-        response[:start_date]   = DateTime.parse(response[:start_date])
-        response[:end_date]     = DateTime.parse(response[:end_date])
+        response[:start_date]   = date_parse(response[:start_date])
+        response[:end_date]     = date_parse(response[:end_date])
         response
       end
 
       def calculate_total(transactions)
         transactions.reduce(0) { |sum, txn| sum + txn[:amount] }
+      end
+
+      def date_parse(string)
+        if string
+          DateTime.parse(string)
+        else
+          nil
+        end
       end
     end
   end
