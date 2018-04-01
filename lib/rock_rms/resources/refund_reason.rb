c3b739ab3acfc30cdf657276d968cb50bc9da525
Taylor Brooks <tbrooks@gmail.com>
2018-03-31 22:13:01 -0500

    Options hash won't be set each time Random uses a range Rubocop styling

--- lib/rock_rms/resources/refund_reason.rb
+++ lib/rock_rms/resources/refund_reason.rb
@@ -2,37 +2,30 @@ module RockRMS
   class Client
     module RefundReason
       def list_refund_reasons(options = {})
+        options['$filter'] = 'DefinedTypeId eq 37' unless options.keys.include?('$filter')
 
-        #
-        # assumes the DefinedType of "Refund Reason" is 37
-        #
-        options = {'$filter' => "DefinedTypeId eq 37"}
-        Response::DefinedValue.format(get(defined_values_path, options))
+        Response::DefinedValue.format(
+          get(defined_values_path, options)
+        )
       end
 
       def create_refund_reason(value:, description:, order: nil)
-
-        order ||= Random.rand(1000) + 1000
+        order ||= Random.rand(100..1000)
 
         options = {
-          'Description' => description,
-          'Value' => value,
-          'Order' => order,
-          'IsSystem' => false,
-          'DefinedTypeId' => 37,
+          'Value'         => value,
+          'Description'   => description,
+          'Order'         => order,
+          'IsSystem'      => false,
+          'DefinedTypeId' => 37
         }
-
-        #
-        # assumes the DefinedType of "Refund Reason" is 37
-        #
-
         post(defined_values_path, options)
       end
 
       private
 
       def defined_values_path
-        "DefinedValues"        
+        'DefinedValues'
       end
     end
   end
