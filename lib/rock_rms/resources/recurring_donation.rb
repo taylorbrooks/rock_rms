d565723821183cdbfc87f518a8f2287f032e58ac
Taylor Brooks <tbrooks@gmail.com>
2018-04-10 16:01:33 -0500

    Add support for creating a recurring donation

--- lib/rock_rms/resources/recurring_donation.rb
+++ lib/rock_rms/resources/recurring_donation.rb
@@ -11,6 +11,36 @@ module RockRMS
         Response::RecurringDonation.format(res)
       end
 
+      def create_recurring_donation(
+        active: true,
+        authorized_person_id:,
+        foreign_key: nil,
+        frequency:,
+        funds:,
+        gateway_id: nil,
+        gateway_schedule_id:,
+        next_payment_date:,
+        source_type_id: 10,
+        transaction_code: nil,
+        start_date:
+      )
+
+        options = {
+          'AuthorizedPersonAliasId': authorized_person_id,
+          'TransactionFrequencyValueId': RECURRING_FREQUENCIES[frequency],
+          'StartDate': start_date,
+          'NextPaymentDate': next_payment_date,
+          'IsActive': active,
+          'FinancialGatewayId': gateway_id,
+          'TransactionCode': transaction_code,
+          'ScheduledTransactionDetails': translate_funds(funds),
+          'GatewayScheduleId': gateway_schedule_id,
+          'SourceTypeValueId': source_type_id,
+          'ForeignKey': foreign_key
+        }
+        post(recurring_donation_path, options)
+      end
+
       def update_recurring_donation(
         id,
         next_payment_date:,
@@ -25,6 +55,24 @@ module RockRMS
       end
 
       private
+
+      RECURRING_FREQUENCIES = {
+        'one-time'  => 130,
+        'weekly'    => 132,
+        'bi-weekly' => 133,
+        'monthly'   => 135,
+        'quarterly' => 153,
+        'yearly'    => 155
+      }.freeze
+
+      def translate_funds(funds)
+        funds.map do |fund|
+          {
+            'Amount' => fund[:amount],
+            'AccountId' => fund[:fund_id]
+          }
+        end
+      end
 
       def recurring_donation_path(id = nil)
         id ? "FinancialScheduledTransactions/#{id}" : 'FinancialScheduledTransactions'
