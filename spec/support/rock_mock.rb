4212bebc959d39410aaf537970a4d5ad01924a4a
Taylor Brooks <tbrooks@gmail.com>
2018-04-13 14:08:20 -0500

    Add support for listing attributes and create an attribute

--- spec/support/rock_mock.rb
+++ spec/support/rock_mock.rb
@@ -19,6 +19,7 @@ class RockMock < Sinatra::Base
 
   # GET requests
   {
+    attributes:    'Attributes',
     batch:         'FinancialBatches/:id',
     batches:       'FinancialBatches',
     campus:        'Campuses/:id',
@@ -46,6 +47,7 @@ class RockMock < Sinatra::Base
 
   # POST requests
   {
+    create_attribute: 'Attributes',
     create_group_member: 'GroupMembers',
     create_transaction: 'FinancialTransactions',
     create_payment_detail: 'FinancialPaymentDetails',
