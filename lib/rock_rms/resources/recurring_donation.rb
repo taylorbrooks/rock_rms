35ffa91f23c5f70c53f105729b9547dbe6f906f5
Taylor Brooks <tbrooks@gmail.com>
2018-06-03 16:36:51 -0500

    Support for twice-a-month recurring frequency

--- lib/rock_rms/resources/recurring_donation.rb
+++ lib/rock_rms/resources/recurring_donation.rb
@@ -57,12 +57,13 @@ module RockRMS
       private
 
       RECURRING_FREQUENCIES = {
-        'one-time'  => 130,
-        'weekly'    => 132,
-        'bi-weekly' => 133,
-        'monthly'   => 135,
-        'quarterly' => 153,
-        'yearly'    => 155
+        'one-time'      => 130,
+        'weekly'        => 132,
+        'bi-weekly'     => 133,
+        'twice-a-month' => 134,
+        'monthly'       => 135,
+        'quarterly'     => 153,
+        'yearly'        => 155
       }.freeze
 
       def translate_funds(funds)
--- lib/rock_rms/version.rb
+++ lib/rock_rms/version.rb
@@ -1,3 +1,3 @@
 module RockRMS
-  VERSION = '4.2.0'.freeze
+  VERSION = '4.2.1'.freeze
 end
