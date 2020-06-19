fb104a23cd94a3916db977232fd08da2110a9b0f
Taylor Brooks <tbrooks@gmail.com>
2020-06-19 17:33:29 -0500

    Nitpick align

--- lib/rock_rms/resources/recurring_donation_detail.rb
+++ lib/rock_rms/resources/recurring_donation_detail.rb
@@ -1,11 +1,10 @@
 module RockRMS
   class Client
     module RecurringDonationDetail
-
       def update_recurring_donation_detail(id, fund_id: nil, amount: nil)
         options = {}
-        options['AccountId'] = fund_id    if fund_id
-        options['Amount'] = amount if amount
+        options['AccountId'] = fund_id if fund_id
+        options['Amount']    = amount  if amount
 
         patch(recurring_donation_detail_path(id), options)
       end
