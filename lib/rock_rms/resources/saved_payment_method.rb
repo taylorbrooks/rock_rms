fa51e96ef562fcec307c9f3e047b77eb10b9e901
Taylor Brooks <tbrooks@gmail.com>
2021-04-24 14:01:55 -0500

    Add gateway_person_id to FinancialSavedPersonAccount

--- lib/rock_rms/resources/saved_payment_method.rb
+++ lib/rock_rms/resources/saved_payment_method.rb
@@ -8,6 +8,7 @@ module RockRMS
 
       def create_saved_payment_method(
         gateway_id:,
+        gateway_person_id: nil,
         payment_detail_id:,
         person_alias_id:,
         name:,
@@ -21,7 +22,32 @@ module RockRMS
           'ReferenceNumber'          => reference_number
         }
 
+        if gateway_person_id
+          options['GatewayPersonIdentifier'] = gateway_person_id
+        end
+
         post(saved_payment_method_path, options)
+      end
+
+      def update_saved_payment_method(
+        id,
+        gateway_id: nil,
+        gateway_person_id: nil,
+        payment_detail_id: nil,
+        person_alias_id: nil,
+        name: nil,
+        reference_number: nil
+      )
+        options = {}
+
+        options['FinancialGatewayId']       = gateway_id        if gateway_id
+        options['GatewayPersonIdentifier']  = gateway_person_id if gateway_person_id
+        options['FinancialPaymentDetailId'] = payment_detail_id if payment_detail_id
+        options['Name']                     = name              if name
+        options['PersonAliasId']            = person_alias_id   if person_alias_id
+        options['ReferenceNumber']          = reference_number  if reference_number
+
+        patch(saved_payment_method_path(id), options)
       end
 
       def delete_saved_payment_method(id)
