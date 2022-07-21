7bab620477fb32ac352802f3c76f3215bb300fc3
Taylor Brooks <taylor@earpventures.com>
2022-07-21 07:29:22 -0500

    Add the ability to delete a gateway

--- lib/rock_rms/resources/gateway.rb
+++ lib/rock_rms/resources/gateway.rb
@@ -1,10 +1,21 @@
 module RockRMS
   class Client
     module Gateway
+      PATH = 'FinancialGateways'.freeze
+
       def list_gateways(options = {})
-        Response::Gateway.format(
-          get('FinancialGateways', options)
-        )
+        res = get(gateway_path, options)
+        Response::Gateway.format(res)
+      end
+
+      def delete_gateway(id)
+        delete(gateway_path(id))
+      end
+
+      private
+
+      def gateway_path(id = nil)
+        id ? "#{PATH}/#{id}" : PATH
       end
     end
   end
