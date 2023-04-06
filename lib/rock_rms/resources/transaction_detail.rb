bfc48fcb140e56cd7f96416859ed6f26fb190197
Josh Crews <josh@joshcrews.com>
2023-04-06 11:30:22 -0400

    create_transaction_detail (#48)

--- lib/rock_rms/resources/transaction_detail.rb
+++ lib/rock_rms/resources/transaction_detail.rb
@@ -31,6 +31,27 @@ module RockRMS
         patch(transaction_detail_path(id), options)
       end
 
+      def create_transaction_detail(
+        transaction_id:,
+        fund_id:,
+        amount:,
+        fee_amount: nil,
+        fee_coverage_amount: nil,
+        entity_type_id: nil,
+        entity_id: nil
+      )
+        options = {}
+        options['TransactionId']     = transaction_id
+        options['AccountId']         = fund_id
+        options['Amount']            = amount
+        options['FeeAmount']         = fee_amount     if fee_amount
+        options['EntityTypeId']      = entity_type_id if entity_type_id
+        options['EntityId']          = entity_id      if entity_id
+        options['FeeCoverageAmount'] = fee_coverage_amount if fee_coverage_amount
+
+        post(transaction_detail_path, options)
+      end
+
       private
 
       def transaction_detail_path(id = nil)
