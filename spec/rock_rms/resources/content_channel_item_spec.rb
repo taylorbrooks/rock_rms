52446c26f02181fb60c1ee0802dc70d8407c5a2b
Nathaniel Jones <nj@third.io>
2024-12-03 14:25:31 -0500

    Content channel item fix

--- spec/rock_rms/resources/content_channel_item_spec.rb
+++ spec/rock_rms/resources/content_channel_item_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-RSpec.describe RockRMS::Client::ContentChannelType, type: :model do
+RSpec.describe RockRMS::Client::ContentChannelItem, type: :model do
   include_context 'resource specs'
 
-  describe '#list_content_channel_types(options = {})' do
-    it 'returns a list of content channel types' do
-      result = client.list_content_channel_types
+  describe '#list_content_channel_items(options = {})' do
+    it 'returns a list of content channel items' do
+      result = client.list_content_channel_items
       expect(result).to be_an(Array)
       expect(result.first).to have_key(:id)
-      expect(result.first).to have_key(:name)
+      expect(result.first).to have_key(:title)
     end
   end
 end
