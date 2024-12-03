7219c83210e8c6261ffc3e0440e4340fb74edf9c
Nathaniel Jones <nathaniel.jones@hey.com>
2024-12-03 18:05:38 -0500

    Content channel item fix (#65)
    Co-authored-by: Nathaniel Jones <nj@third.io>

--- lib/rock_rms/resources/content_channel_item.rb
+++ lib/rock_rms/resources/content_channel_item.rb
@@ -2,7 +2,7 @@ module RockRMS
   class Client
     module ContentChannelItem
       def list_content_channel_items(options = {})
-        res = get(content_channel_items_path, options)
+        res = get('ContentChannelItems', options)
         Response::ContentChannelItem.format(res)
       end
     end
