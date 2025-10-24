ab347f5fcfa3519d810967872a1403668144787a
Josh Crews <josh@joshcrews.com>
2025-10-24 16:04:47 -0500

    accept entity connection on update/create of a recurring donation detail (#69)

--- lib/rock_rms/resources/recurring_donation_detail.rb
+++ lib/rock_rms/resources/recurring_donation_detail.rb
@@ -6,6 +6,8 @@ module RockRMS
       def create_recurring_donation_detail(
         recurring_donation_id:,
         fee_coverage_amount: nil,
+        entity_type_id: nil,
+        entity_id: nil,
         fund_id:,
         amount:
       )
@@ -16,6 +18,8 @@ module RockRMS
         }
 
         options['FeeCoverageAmount'] = fee_coverage_amount if fee_coverage_amount
+        options['EntityTypeId'] = entity_type_id if entity_type_id
+        options['EntityId'] = entity_id if entity_id
 
         post(recurring_donation_detail_path, options)
       end
@@ -24,11 +28,13 @@ module RockRMS
         delete(recurring_donation_detail_path(id))
       end
 
-      def update_recurring_donation_detail(id, fund_id: nil, amount: nil, fee_coverage_amount: nil)
+      def update_recurring_donation_detail(id, fund_id: nil, amount: nil, fee_coverage_amount: nil, entity_type_id: nil, entity_id: nil)
         options = {}
         options['AccountId'] = fund_id if fund_id
         options['Amount']    = amount  if amount
         options['FeeCoverageAmount'] = fee_coverage_amount if fee_coverage_amount
+        options['EntityTypeId'] = entity_type_id if entity_type_id
+        options['EntityId'] = entity_id if entity_id
 
         patch(recurring_donation_detail_path(id), options)
       end
